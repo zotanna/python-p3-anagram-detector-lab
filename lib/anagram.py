f968b3acb07146691d5ac8a29c6e66813a8ba22a
denis <denniiskim@gmail.com>
2024-01-17 04:22:45 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,9 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, word_list):
+        return [w for w in word_list if self._are_anagrams(w)]
+
+    def _are_anagrams(self, other_word):
+        return sorted(self.word) == sorted(other_word.lower())
